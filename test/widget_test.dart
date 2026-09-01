1385e45cb81951d6efcec278978937f9ed21b517
Birang Pawala <birangmodi9327@gmail.com>
2026-09-01 11:16:17 +0530

    initialize basic app core

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:lsa_verification_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LsaVerificationApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
